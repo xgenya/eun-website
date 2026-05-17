1edbf4967a9fb5eb026b6d185f99b0795015bd0b
xgenya <29149596+xgenya@users.noreply.github.com>
2026-05-17 22:16:50 +0800

    mod: mod teams user desc
    Signed-off-by: xgenya <29149596+xgenya@users.noreply.github.com>

--- config/team.ts
+++ config/team.ts
@@ -30,18 +30,18 @@ const team = {
   // 核心玩家列表（展示于 About 页面次要位置）
   coreMembers: [
     { name: "sixteen0522", username: "sixteen0522", color: "#7090e0", desc: "湖南搞机王" },
-    { name: "motor10086", username: "motor10086", color: "#e07878", desc: "" },
-    { name: "Y_jiaopiYa", username: "Y_jiaopiYa", color: "#c070c0", desc: "" },
-    { name: "zongcheng", username: "zongcheng", color: "#70c0c0", desc: "" },
+    { name: "motor10086", username: "motor10086", color: "#e07878", desc: "AAA搞机mo师傅" },
+    { name: "Y_jiaopiYa", username: "Y_jiaopiYa", color: "#c070c0", desc: "从小会独立呼吸，会玩智能手机" },
+    { name: "zongcheng", username: "zongcheng", color: "#70c0c0", desc: "偶尔在地球兼职人类" },
     { name: "dxh5201314", username: "dxh5201314", color: "#e0a060", desc: "天才少年，被可恶的高中生活所绑架" },
     { name: "zqrbobo", username: "zqrbobo", color: "#5db85d", desc: "全服最善良(?)的“小猫咪”，人见人爱，乐于帮助新人融入大家庭，有任何困难都可以向他寻求帮助" },
-    { name: "Zerostar_0", username: "Zerostar_0", color: "#c0c060", desc: "" },
-    { name: "Rhandoms", username: "Rhandoms", color: "#c08060", desc: "" },
+    { name: "Zerostar_0", username: "Zerostar_0", color: "#c0c060", desc: "养号的话一定要超过15个字儿，不懂为什么，但是要听前辈们的忠告" },
+    { name: "Rhandoms", username: "Rhandoms", color: "#c08060", desc: "谁敢成无敌，何人敢言不败？" },
     { name: "XTDFG", username: "XTDFG", color: "#60a0e0", desc: "全服最舒服之人，和我一起玩很舒服" },
-    { name: "Tastemedicine", username: "Tastemedicine", color: "#e07878", desc: "" },
+    { name: "Tastemedicine", username: "Tastemedicine", color: "#e07878", desc: "嘿嘿（这是严肃思考一晚上想出来的个人介绍）" },
     { name: "canling_CL", username: "canling_CL", color: "rgba(170, 47, 170, 0.67)", desc: "创造老登" },
     { name: "OV_E", username: "OV_E", color: "rgba(47, 156, 10, 0.67)", desc: "摸鱼大王，挂机大神" },
-    { name: "Timo0V0", username: "Timo0V0", color: "rgba(15, 62, 109, 1)", desc: "" },
+    { name: "Timo0V0", username: "Timo0V0", color: "rgba(15, 62, 109, 1)", desc: "死亡榜第一人" },
     { name: "Warm1Sad", username: "Warm1Sad", color: "rgba(13, 174, 23, 1)", desc: "我痞吗" }
   ],
 };
